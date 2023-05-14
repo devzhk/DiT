2e59a9ed3d58bd2182c216da399e173595f61820
devzhk <devzhk@gmail.com>
2023-05-13 22:12:38 -0700

    add eval jobs

---
+++ scripts/eval-latent-1024.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+pip install einops lmdb omegaconf wandb tqdm pyyaml accelerate
+pip install git+https://github.com/huggingface/pytorch-image-models.git
+pip install git+https://github.com/huggingface/diffusers
+torchrun --nnodes=1 --nproc_per_node=8 sample_ddp.py --ckpt_dir results/003-DiT-XL-2/checkpoints  --id_min 50000 --id_max 250000 --id_step 50000
