030d0166aa148c76025abb84b2e2d62310cbca43
devzhk <devzhk@gmail.com>
2023-05-15 10:00:47 -0700

    update scripts

--- scripts/eval-latent.sh
+++ scripts/eval-latent.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-pip install einops lmdb omegaconf wandb tqdm pyyaml diffusers accelerate transformers
+pip install einops lmdb omegaconf wandb tqdm pyyaml accelerate transformers diffusers
 pip install git+https://github.com/huggingface/pytorch-image-models.git
-torchrun --nnodes=1 --nproc_per_node=8 sample_ddp.py --ckpt_dir results/002-DiT-XL-2/checkpoints  --id_min 100000 --id_max 1300000 --id_step 100000
+torchrun --nnodes=1 --nproc_per_node=8 sample_ddp.py --ckpt_dir results/002-DiT-XL-2/checkpoints  --id_min 100000 --id_max 1400000 --id_step 100000
